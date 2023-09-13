7a2c06cdf827d5ebe81fb95bbbe85ffacd5aedc5
Katsiara <katsiara@mail.ru>
2023-09-13 23:28:19 +0300

    ДЗ

---
+++ task66hw/Program.cs
@@ -0,0 +1,31 @@
+﻿// Задайте значения M и N. 
+// Напишите программу, которая найдёт сумму
+//  натуральных элементов в промежутке от M до N.
+// M = 1; N = 15 -> 120
+// M = 4; N = 8. -> 30
+
+
+
+Console.WriteLine("Введите натуральное число:");
+int number1 = Convert.ToInt32(Console.ReadLine());
+
+Console.WriteLine("Введите натуральное число:");
+int number2 = Convert.ToInt32(Console.ReadLine());
+
+
+int SumOfNaturalNumbers(int num1, int num2)
+{
+    if (num1 == num2) return num2;
+    else if (num1<num2)
+    {
+      return  num1 + SumOfNaturalNumbers(num1+1, num2);
+    }
+    else 
+    {
+       return num2 + SumOfNaturalNumbers(num2+1, num1); 
+    }
+
+} 
+
+Console.WriteLine($"{SumOfNaturalNumbers(number1, number2)}");
+
---
+++ task68hw/Program.cs
@@ -0,0 +1,40 @@
+﻿// Напишите программу вычисления функции Аккермана
+//  с помощью рекурсии. 
+//  Даны два неотрицательных числа m и n.
+// m = 2, n = 3 -> A(m,n) = 9
+// m = 3, n = 2 -> A(m,n) = 29
+
+
+int Prompt(string message)
+{
+    Console.Write(message);
+    int value = Convert.ToInt32(Console.ReadLine());
+    return value;
+}
+
+int Validation(int num)
+{
+    while (num < 1)
+    {
+        num = Prompt("Неверный ввод");
+    }
+    return num;
+}
+
+int Ackerman(int num1, int num2)
+{
+    if (num1 == 0) return num2 + 1;
+    else if (num2 == 0) return Ackerman(num1 - 1, 1);
+    else
+    {
+        return Ackerman(num1 - 1, Ackerman(num1, num2 - 1));
+    }
+}
+
+Console.WriteLine("Введите неотрицательное число:");
+int number1 = Convert.ToInt32(Console.ReadLine());
+Console.WriteLine("Введите неотрицательное число:");
+int number2 = Convert.ToInt32(Console.ReadLine());
+number1 = Validation(number1);
+number2 = Validation(number2);
+Console.WriteLine($"Функция Аккермана равна {Ackerman(number1, number2)}");
